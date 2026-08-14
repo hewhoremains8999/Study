223a67f000ceb668de333c1c886582983ad75641
Loki <hewhoremains8999@gmail.com>
2026-08-13 19:55:40 -0700

    First UI

---
+++ Java/calc/Ui.java
@@ -0,0 +1,11 @@
+import javax.swing.*; //Gui component dete hai like JFrame,JButton,JTextFeild
+import java.awt.*;    //Helps in layout of Gui
+import java.awt.event.*;  //Helps in event handling of user input
+
+public class Ui {
+public static void main(String[] args) {
+    JFrame f =new JFrame("First UI");
+    f.setSize(255,255);
+    f.setVisible(true);
+}    
+}
